0374c797d3865444d3879572aead5c1a40fcdf24
amilcarrey <amilcarrey.ar@gmail.com>
2023-04-21 18:25:49 -0300

    fix build

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,15 @@
+/* eslint-disable no-console */
 import { LocationType, Prisma } from '@prisma/client'
-import { prisma } from '../src/db'
+import { prisma } from '@/utils/db'
 
-const contractData : Prisma.ContractsCreateInput = {
+const contractData: Prisma.ContractsCreateInput = {
    name: 'Contract 1',
 }
-const locationConfigData : Prisma.LocationConfigsCreateInput = {
+const locationConfigData: Prisma.LocationConfigsCreateInput = {
    name: 'Location Config 1',
    locationId: 1
 }
-const locationData : Prisma.LocationsUncheckedCreateInput = {
+const locationData: Prisma.LocationsUncheckedCreateInput = {
    name: 'Location 1',
    latitude: 1,
    longitude: 1,
@@ -17,7 +18,7 @@ const locationData : Prisma.LocationsUncheckedCreateInput = {
    locationType: LocationType.NEIGHBORHOOD
 }
 
-const functionalUnitData  = {
+const functionalUnitData = {
    ciu: '5321a',
    access_code: '123',
    locationId: 1,
@@ -25,30 +26,30 @@ const functionalUnitData  = {
 
 async function main() {
    console.log(`Start seeding ...`)
-   
+
    await prisma.contracts.deleteMany()
-    console.log('Deleted records in contracts table')
-    
+   console.log('Deleted records in contracts table')
+
    await prisma.locationConfigs.deleteMany()
    console.log('Deleted records in locationConfigs table')
 
-    await prisma.locations.deleteMany()
-    console.log('Deleted records in locations table')
+   await prisma.locations.deleteMany()
+   console.log('Deleted records in locations table')
 
-    await prisma.functionalUnits.deleteMany()
-    console.log('Deleted records in functionalUnits table')
+   await prisma.functionalUnits.deleteMany()
+   console.log('Deleted records in functionalUnits table')
 
-    await prisma.$queryRaw`ALTER TABLE Contracts AUTO_INCREMENT = 1`
-    console.log('reset Contracts auto increment to 1')
-    
+   await prisma.$queryRaw`ALTER TABLE Contracts AUTO_INCREMENT = 1`
+   console.log('reset Contracts auto increment to 1')
+
    await prisma.$queryRaw`ALTER TABLE LocationConfigs AUTO_INCREMENT = 1`
    console.log('reset LocationConfigs auto increment to 1')
 
-    await prisma.$queryRaw`ALTER TABLE Locations AUTO_INCREMENT = 1`
-    console.log('reset Locations auto increment to 1')
+   await prisma.$queryRaw`ALTER TABLE Locations AUTO_INCREMENT = 1`
+   console.log('reset Locations auto increment to 1')
 
-    await prisma.$queryRaw`ALTER TABLE FunctionalUnits AUTO_INCREMENT = 1`
-    console.log('reset FunctionalUnits auto increment to 1')
+   await prisma.$queryRaw`ALTER TABLE FunctionalUnits AUTO_INCREMENT = 1`
+   console.log('reset FunctionalUnits auto increment to 1')
 
    const contract = await prisma.contracts.create({ data: contractData })
    console.log(`Created user with id: ${contract.id}`)
